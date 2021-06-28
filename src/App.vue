704cf7162f9430384d9406a41d0b27aaff50cc07
Lucas Colombo <lucas.c.hurpia@gmail.com>
2021-06-28 13:10:56 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <Header title="Task Tracker" />
+    <Tasks :tasks="tasks" />
+  </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import Tasks from "./components/Tasks.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Header,
+    Tasks,
+  },
+
+  data() {
+    return {
+      tasks: [],
+    };
+
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "taks1",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "taks2",
+        day: "March sft at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "taks3",
+        day: "March 1st at 2:30pm",
+        reminder: false,
+      },
+    ];
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+@import url('./components/StyleSheets/Style.css');
 </style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+  <span>
+    <div :key="task.id" v-for="task in tasks">
+      <Task :task="task" />
+    </div>
+  </span>
+</template>
+
+<script>
+import Task from "./Task.vue";
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: {
+    Task,
+  },
+};
+</script>
+
+
